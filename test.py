6f8f31e4c0717866169cd0195566c0122d201bc5
Arina Karpan <arias.workplace@gmail.com>
2022-07-03 12:32:11 +0200

    типы данных

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
-number = 3
-number = 4
-number2 = 8
-result = number + number2
-print(result)
-
-num1 = num2 = 5
-print(num1, num2)
-
-num_1, num_2 = 5, 7
-print(num_1, num_2)
-
-swap1 = 8
-swap2 = 9
-swap1, swap2 = swap2, swap1 + swap2
-print(swap1, swap2)
-
-swap2 -= number
-print(swap2)
-
-z, x, *с = [1, 2, 3, 4, 5]
-print(z)
-print(x)
-print(с)
+a = None            # отсутствие данных
+print(type(a))
+a = 1               # целое число
+print(type(a))
+a = 1.0             # число с плавающей точкой
+print(type(a))
+a = 1 + 1j          # комплексное число
+print(type(a))
+a = '1'             # строка 
+print(type(a))
+a = [1, 1, 'a']     # список
+print(type(a))
+a = (1, 1, 'a')     # картеж
+print(type(a))
+a = {1, 2, 'a'}     # множество
+print(type(a))
+a = {'a':1, 'b':2}  # словарь
+print(type(a))
+a = True            # булевое значение
+print(type(a))
