de21e4a68c43bba3cd5f6d2dc41019371b016456
Arina Karpan <arias.workplace@gmail.com>
2022-07-04 16:27:51 +0200

    небольшая задачка

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-if False:
-    print('if')
-elif False:          #else + if
-    print('elif')
+x = 'a'
+
+if x == 0:
+    x = 1
+    print('x был равен нулю')
+
+elif type(x) == type(5) or type(x) == type(5.5):
+    print('x допустимое значение')
+
 else:
-    print('else')
+    print('В x недопустимый тип данных')
+    x = 1
+
+
+
+print(100/x)
