d6006a9f4f85452a486dfbca0e2fd862692a1289
Arina Karpan <arias.workplace@gmail.com>
2022-07-04 16:05:42 +0200

    условные операторы

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-x = input('5 + ')  #input место для введения
-y = float(x) + 5   #int переводит строку в число;float - в число с плавающей точкой
-print(y)
-
-a = float(input('Введите число '))
-b = float(input('Введите число '))
-c = a + b
-print('Результат: ' + str(c)) #str переводит число в строку
+if False:
+    print('if')
+elif False:          #else + if
+    print('elif')
+else:
+    print('else')
